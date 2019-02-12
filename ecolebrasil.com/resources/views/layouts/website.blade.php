a5374b1abbdc5fd178a4081258fb3df310f77123
Alberto <alberto.pimentel.94@gmail.com>
2019-02-12 16:34:52 -0200

    erer

--- ecolebrasil.com/resources/views/layouts/website.blade.php
+++ ecolebrasil.com/resources/views/layouts/website.blade.php
@@ -248,5 +248,15 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   });
 </script>
 @yield('content-js')
+<!-- BEGIN JIVOSITE CODE {literal} -->
+<script type='text/javascript'>
+(function(){ var widget_id = '3YOaznX6u0';var d=document;var w=window;function l(){
+  var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+  s.src = '//code.jivosite.com/script/widget/'+widget_id
+    ; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}
+  if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}
+  else{w.addEventListener('load',l,false);}}})();
+</script>
+<!-- {/literal} END JIVOSITE CODE -->
 </body>
 </html>
